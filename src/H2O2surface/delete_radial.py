296c4f877c383a8f81a1e419200826370c4796aa
OtsukiRiku <riku.otsuki.q4@dc.tohoku.ac.jp>
2021-10-25 13:05:17 +0900

    del_radi

---
+++ src/H2O2surface/delete_radial.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from MolCop import mmpystream as mmps
+from MolCop.analysis import topology
+
+ss = mmps.Stream()
+
+ss.import_file('show_dump', 'dumppos')
+ss.import_file('dump.bond.0', 'dumpbond')
+
+ss.sdat.create_connect_list(0.3)
+connect_list = ss.sdat.connect_list
+m_list = topology.create_molecule(ss.sdat)
+
+ss.sdat.add_particles_property("molnum", _dtype=int, dim=1)
+for l in m_list:
+    num_mol = len(l)
+    for ind in l:
+        ss.sdat.particles["molnum"][ind]=num_mol
+print(ss.sdat.particles["molnum"])
+flag = ss.sdat.particles["molnum"] > 2
+ss.sdat.trimming_particles(flag)
+
+"""
+for i in range(0,len(ss.sdat.particles['mol'])):
+    ID = ss.sdat.particles['pos'].count_nonzero(i < 3)
+print(ID)
+"""
+ss.output_file('test.pos', 'dumppos', ['id','type', 'mol', 'pos'])
+
+
