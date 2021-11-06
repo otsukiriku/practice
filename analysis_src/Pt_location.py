9778568d07d6b863f8af052e4851752c0befb1b3
OtsukiRiku <riku.otsuki.q4@dc.tohoku.ac.jp>
2021-11-06 20:01:02 +0900

    Pt_location

--- analysis_src/Pt_location.py
+++ analysis_src/Pt_location.py
@@ -30,8 +30,8 @@ import numpy as np
 import sys
 import copy
 import math
-CB_num = 2
-Pt_num = 20
+CB_num = 4
+Pt_num = 56
 Pt_cluster_pnum = 309
 
 ss=mmps.Stream()
@@ -69,14 +69,22 @@ for mask in CB_list:
     CB['pos'][flag]+=pos_shift
 
     flag = CB["mask"] == mask
+    #print(CB["mask"]) 
+    #print(flag) 
     CB_g = [CB["pos"][flag].mean(axis=0)]
     CB_G = np.append(CB_G, CB_g, axis=0)
+a=CB_num-1
+bottom = CB_G[CB_num-1,] - np.array(pos_shift)
+CB_G = np.append(CB_G, bottom)
+top = CB_G[0,] + np.array(pos_shift)
+CB_G = np.append(CB_G, top)
+CB_G = np.reshape(CB_G,(CB_num+2,3))
 #print("CB_G")
 #print(CB_G)
 
 #get each Ptcluster gravity center
 Pt=ss.sdat
-Pt.create_connect_list(0.3)
+#Pt.create_connect_list(0.3)
 #c_list = Pt.connect_list
 #m_list = topology.create_molecule(Pt)
 Pt_list=[i for i in range(7,7+Pt_num)]
@@ -93,48 +101,53 @@ Ptmask = Ptmask.flatten()
 ss.sdat.particles['mask'][Pt.particles['type'] == 4] = Ptmask
 
 Pt_list=np.array(Pt_list)
-print(Pt_list)
+#print(Pt_list)
 Pt_G = np.array([g_c_by_mask(Pt.particles, l) for l in Pt_list])
 #print("Pt_G")
 #print(Pt_G)
 
 #get closest & 2nd closest dis from center
-m_l1 = [i+1 for i in range(0,CB_num)]
+# original
+#m_l1 = [i+1 for i in range(0,CB_num)]
+m_l1 = [i+1 for i in range(0,CB_num+2)]
 mask_l = [m_l1 for j in range(Pt_num)]
 Pt_CB_dis=np.array([get_between_dis(j, CB_G) for j in Pt_G])
 #print("Pt_CB_dis")
 #print(Pt_CB_dis)
-
 #arguments of mask of closest CB from Pt
-argsort = np.argsort(Pt_CB_dis, axis=1)
-
+sort = np.sort(Pt_CB_dis, axis=1)
+print(sort)
 s_mask=np.empty((0,1), dtype=int)
 s_dis=np.empty((0,1))
 l_mask=np.empty((0,1),dtype=int)
 l_dis=np.empty((0,1))
 c_dis=np.empty((0,1))
-for idx, arg in enumerate(argsort):
+for idx, arg in enumerate(sort):
     #各Ptから最小のCBのmaskを取り出す．
-    s_mask_idx = np.where(arg==0)[0][0]
+    #print( np.where(Pt_CB_dis[idx:idx+1][0]==arg[0])[0][0])
+    s_mask_idx = np.where(Pt_CB_dis[idx,:]==arg[0])[0][0]
     s_dis = np.append(s_dis,Pt_CB_dis[idx,s_mask_idx])
     s_mask = np.append(s_mask, s_mask_idx)
     #二番目に距離が短いmaskとかを取り出す．
-    l_mask_idx = np.where(arg==1)[0][0]
+    l_mask_idx = np.where(Pt_CB_dis[idx,:]==arg[1])
     l_dis = np.append(l_dis,Pt_CB_dis[idx,l_mask_idx])
     l_mask = np.append(l_mask, l_mask_idx)
     #近い二つCBの中心間距離c_dis
-    dis = ( CB_G[s_mask_idx] - CB_G[l_mask_idx])**2
-    disarr = np.sum(dis,axis = 0)
-    c_dis = np.append(c_dis, disarr)
     #Ptの重心から最近接，第二近接CBの中間の座標までの距離を出す
     CB_between_pos = ( CB_G[s_mask_idx] + CB_G[l_mask_idx] )/2
     Pt_to_CBbet = CB_between_pos - Pt_G
+    
+dis = ( CB_G[s_mask,:] - CB_G[l_mask,:])**2
+c_dis = np.sum(dis,axis = 1)
+#c_dis = np.append(c_dis, disarr)
 
 s_dis=np.sqrt(s_dis)
 l_dis=np.sqrt(l_dis)
 c_dis=np.sqrt(c_dis)
-print("s_mask")
-print(s_mask)
+#print("s_mask")
+#print(s_mask)
+#print("l_mask")
+#print(l_mask)
 #print("s_dis")
 #print(s_dis)
 #print("l_dis")
@@ -143,17 +156,19 @@ print(s_mask)
 #print(c_dis)
 
 #prove radius
-CB_to_Pt_vec=((Pt_G-CB_G[s_mask]))
+CB_to_Pt_vec=((Pt_G-CB_G[s_mask,:]))
+#print(CB_to_Pt_vec)
 pr = s_dis*c_dis**2/(-l_dis**2+s_dis**2+c_dis**2) - s_dis
-correct = np.sum(s_dis)/Pt_num-100
-pr = pr + correct
 #print("prove radius")
 #print(pr)
 
 #get prove center(for test)
 scalor=(s_dis+pr)/s_dis
 scalor=np.reshape(scalor, (Pt_num,1))
-pr_pos = CB_G[s_mask] + CB_to_Pt_vec*(scalor)
+correct = np.sum(s_dis)/Pt_num-100
+pr = pr + correct
+pr_pos = CB_G[s_mask,:] + CB_to_Pt_vec*(scalor)
+#print(CB_G[s_mask])
 #print("pr_pos")
 #print(pr_pos)
 """
@@ -168,7 +183,7 @@ count = []
 [count.append(i) for i in range(0,100,10)]
 freq = []
 mask = []
-#Pt2CBbet = []
+Pt2CBbet = []
 #for showing probe
 pr_list = []
 pr_pos_list = []
@@ -180,29 +195,49 @@ for co in count:
             mask.append(Pt_list[idx])
             pr_list.append(pr[idx])
             pr_pos_list.append(pr_pos[idx])
-            #Pt2CBbet.append(Pt_to_CBbet[idx])
+            Pt2CBbet.append(Pt_to_CBbet[idx])
             ss1.sdat.particles['id']=np.append(ss1.sdat.particles['id'],idx)
             ss1.sdat.particles['pos']=np.append(ss1.sdat.particles['pos'],[pr_pos[idx]],axis=0)
             ss1.sdat.particles['type']=np.append(ss1.sdat.particles['type'],Pt_list[idx])
     freq.append(pr_num)
 #print(freq)
+
 print("Ptmask_list")
 print(mask)
 print("pr_radius_list")
 print(pr_list)
 print("pr_pos_list")
 [print(i) for i in pr_pos_list]
-#print("Pt to CB between pos")
-#[print(i) for i in Pt2CBbet]
+print("Pt to CB between pos")
+[print(i) for i in Pt2CBbet]
 print("prove_radius_range freq")
 [print(f'{f}~{f+10} {c}', sep=' ', end="\n" ) for f, c in zip(count, freq)]
 
 #print(ss.sdat.particles['pos'])
 
 
-"""debug show probe radius
+#debug show probe radius
 ss.sdat.concate_particles(ss1.sdat.particles)
-print(ss.sdat.particles['id'])
 #ss.output_file('test_input', 'input')
-ss.output_file('probe_dump', 'dumppos',['id', 'type', 'pos'])
+ss.sdat.add_particles_property("mol", int, 1)
+ss.sdat.particles["mol"][ss.sdat.particles["type"]==4] = Ptmask
+ss.output_file('probe_dump', 'dumppos',['id', 'type', 'pos','mol'])
+
+
+"""
+#debug show Pt_G
+for idx, r in enumerate(Pt_G):
+    pr_num+=1
+    mask.append(Pt_list[idx])
+    pr_list.append(pr[idx])
+    pr_pos_list.append(pr_pos[idx])
+    Pt2CBbet.append(Pt_to_CBbet[idx])
+    ss1.sdat.particles['id']=np.append(ss1.sdat.particles['id'],idx)
+    ss1.sdat.particles['pos']=np.append(ss1.sdat.particles['pos'],[r],axis=0)
+    ss1.sdat.particles['type']=np.append(ss1.sdat.particles['type'],10)
+
+
+ss.sdat.concate_particles(ss1.sdat.particles)
+#ss.output_file('test_input', 'input')
+ss.output_file('Pt_G_dump', 'dumppos',['id', 'type', 'pos'])
 """
