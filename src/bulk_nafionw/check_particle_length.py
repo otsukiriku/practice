89f6e6a99ef425da28d0122aa9317503a2c6326a
OtsukiRiku <riku.otsuki.q4@dc.tohoku.ac.jp>
2021-12-01 00:27:18 +0900

    check_particle_length

--- src/bulk_nafionw/check_particle_length.py
+++ src/bulk_nafionw/check_particle_length.py
@@ -4,86 +4,57 @@ import sys
 from MolCop import mmpystream as mmps
 from MolCop.analysis import topology
 
-ss = mmps.Stream()
-ifn = sys.argv[1]
-ss.import_file(ifn, 'dumppos')
-ifn = sys.argv[2]
-ss.import_file(ifn, 'dumpbond')
-
-ss.sdat.set_elem_to_type(mmps.get_elem_to_type("./para.rd"))
-
-ss.sdat.create_connect_list(0.3)
-connect_list = ss.sdat.connect_list
-m_list = topology.create_molecule(ss.sdat)
-
-ss.sdat.add_particles_property("molnum", _dtype=int, dim=1)
-for l in m_list:
-    num_mol = len(l)
-    for ind in l:
-        ss.sdat.particles["molnum"][ind]=num_mol
-print(ss.sdat.particles["molnum"])
-
-mn_list = []
-for i in range(400,430):
-    mn_list.append(i)
-
-print("mn_list")
-print(mn_list)
-flag_2d = np.zeros((1,len(ss.sdat.particles['id'])), dtype=int)
-for i in mn_list:
-    flag = ss.sdat.particles["molnum"] == i
-    print(flag)
-    flag = flag.astype(int)
-    flag_2d = np.append(flag_2d, [flag], axis=0)
-flag1d = np.sum(flag_2d, axis=0) != 0
-#print(flag1d)
-
-#print(len(ss.sdat.particles['id'][ss.sdat.particles["molnum"]==416]))
-print(len(ss.sdat.particles["id"][flag1d])/416)
-
-m_n = ss.sdat.particles["molnum"] == 3
-print("number of water particles")
-print(len(ss.sdat.particles['id'][m_n])/3)
-
-"""
-#delete H, OHterminal in inside of CB
-CB_mask=[]
-CB_g=[]
-center=open("./center.txt", mode='r')
-line = center.readline()
-while True:
-    line = center.readline().split()
-    if not line:
-        break
-    CB_mask.append(line.pop(0))
-    CB_g.append(line)
-    print(line)
-CB_mask=list(map(int, CB_mask))
-#print(CB_mask)
-CB_G=[]
-for line in CB_g:
-    CB_G.append(list(map(float, line)))
-CB_G=np.array(CB_G)
-#print(CB_G)
-center.close()
-
-for cent in CB_G:
-    H_flag = (ss.sdat.particles['type'] != 2)|(ss.sdat.particles['type'] != 3) 
-    dist = (ss.sdat.particles['pos']-cent)**2
-    distarr=np.sum(dist, axis=1)
-    dist_flag = distarr>9500
-    flag = H_flag & dist_flag
-    print(flag)
-    ss.sdat.trimming_particles(flag)
+def p_len(atoms : mmps.Stream().sdat, target_len=None, _UpperCutOff = 0 , _LowerCutOff = 0):
     
-"""
+    if target_len is None:
+        print("You need to set Target length of particle!")
+        exit()
+    else:
+        atoms.create_connect_list(0.3)
+        connect_list = atoms.connect_list
+        m_list = topology.create_molecule(atoms)
 
-"""
-for i in range(0,len(ss.sdat.particles['mol'])):
-    ID = ss.sdat.particles['pos'].count_nonzero(i < 3)
-print(ID)
-"""
-#ss.output_file('withwaterlayer.dump', 'dumppos', ['id','type','pos','velo','mask'])
-#ss.output_file("withwaterlayer.rd", 'input')
+        atoms.add_particles_property("molnum", _dtype=int, dim=1)
+        for l in m_list:
+            num_mol = len(l)
+            for ind in l:
+                atoms.particles["molnum"][ind]=num_mol
+        
+        print("List of molecule length")
+        m_n = list(set(atoms.particles["molnum"]))
+        m_n.sort()
+        print(m_n)
 
+        mn_list = []
+        if _UpperCutOff != 0 | _LowerCutOff != 0:
+            for i in range(target_len - _LowerCutOff , target_len + _UpperCutOff +1):
+                mn_list.append(i)
+        else:
+            mn_list = [target_len]
 
+        flag_2d = np.zeros((1,len(atoms.particles['id'])), dtype=int)
+        for i in mn_list:
+            flag = atoms.particles["molnum"] == i
+            #print(flag)
+            flag = flag.astype(int)
+            flag_2d = np.append(flag_2d, [flag], axis=0)
+        flag1d = np.sum(flag_2d, axis=0) != 0
+        #print(flag1d)
+
+        print("Number of Target particles is ...")
+        print(len(atoms.particles["id"][flag1d])/target_len)
+        
+if __name__ == '__main__':
+    ss = mmps.Stream()
+    ifn = sys.argv[1]
+    ss.import_file(ifn, 'dumppos')
+    ifn = sys.argv[2]
+    ss.import_file(ifn, 'dumpbond')
+    #example target molecule is Nafion (Molecule is composed of 416 particles)
+    t_len = 416
+    #Nafion has 6 sidechains, Assuming all sidechain coordinate with water, 3×6 = 18
+    upper = 18
+    #Nafion has 6 sidechains, Assuming H particle away from all side chains 1×6 = 6
+    lower = 6
+    p_len(ss.sdat ,target_len=t_len, _UpperCutOff=upper, _LowerCutOff=lower)
+    
